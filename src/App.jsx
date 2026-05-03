4a002b9089a79e5834b3563daa076c871e098c53
echo <jerichobaal1730@gmail.com>
2026-05-03 11:55:57 +0800

    add: Speed insights vercel, update:cv asset

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import Navbar from "./components/Navbar/Navbar";
 import "./App.css"
 import Hero from "./components/Hero/Hero";
@@ -21,7 +22,7 @@ return (
         <Contact/>
         <Footer/>
         <Analytics/>
-        
+        <SpeedInsights/>
     </section>
 )
 }
